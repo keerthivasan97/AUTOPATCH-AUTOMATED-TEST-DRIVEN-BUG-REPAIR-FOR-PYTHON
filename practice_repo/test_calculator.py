19c5974805fba2c4568070fca01b8e30d9e673aa
keerthivasan97 <kerthivasan.s2024@vitstudent.ac.in>
2026-07-27 09:00:02 +0530

    Update test cases

--- practice_repo/test_calculator.py
+++ practice_repo/test_calculator.py
@@ -1,6 +1,7 @@
 from calculator import divide
 import pytest
 def test_divide():
-    assert divide(10,2) ==5
+    assert divide(10,3) == pytest.approx(3.33333, abs=3.4e-06)
+    assert divide(0,2) == 0,"0/2= 0"
     with pytest.raises(ValueError,match="cannot divide by zero"):
         divide(10,0)
